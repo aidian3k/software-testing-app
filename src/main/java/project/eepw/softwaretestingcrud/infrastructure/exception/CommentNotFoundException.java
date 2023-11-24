ec8ee9ffcaca87a1a063932d3814259a21a23c5c
dobos <doboszdav@gmail.com>
2023-11-24 21:01:07 +0100

    comment logic fix

---
+++ src/main/java/project/eepw/softwaretestingcrud/infrastructure/exception/CommentNotFoundException.java
@@ -0,0 +1,12 @@
+package project.eepw.softwaretestingcrud.infrastructure.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class CommentNotFoundException extends RuntimeException {
+
+  public CommentNotFoundException(String message) {
+    super(message);
+  }
+}
